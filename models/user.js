d0994e788539a2a30fdf43494357d2e3b7c4082d
Nate Snyder <n8.snyder@berkeley.edu>
2022-02-21 11:56:36 -0800

    Wrote register for User model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 "use strict";
 
-/** User of the site. */
+const bcrypt = require("bcrypt");
 
+const db = require("../db");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
+/** User of the site. */
 class User {
 
   /** Register new user. Returns
@@ -9,6 +13,14 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone)
+        VALUES
+          ($1, $2, $3, $4, $5)
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]);
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
