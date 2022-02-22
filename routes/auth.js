fe7f9eda01d6fd030d45192d1708ee6e039468f6
Nate Snyder <n8.snyder@berkeley.edu>
2022-02-21 16:23:48 -0800

    Started working on auth/login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,23 @@
 "use strict";
 
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError } = require("../expressError");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 const Router = require("express").Router;
 const router = new Router();
 
 /** POST /login: {username, password} => {token} */
 
+router.post("/login", function (req, res, next) {
+  const { username, password } = req.body;
+  if (User.authenticate(username, password)) {
+    console.log("AUTHENTICATED!");
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.send({ token });
+  }
+  throw new UnauthorizedError();
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
